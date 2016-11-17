af21624bd6574e89e00fd751613a6e1ad378d641
CLazStudio <clazstudio@gmail.com>
2016-11-17 22:59:32 +0600

    Add Main menu. Change edit menu. Add options and loadout

--- lua/autorun/weaponsets.lua
+++ lua/autorun/weaponsets.lua
@@ -2,7 +2,11 @@ wepsets = wepsets or {}
 wepsets.netFuncs = wepsets.netFuncs or {}
 
 if ( SERVER ) then
+    AddCSLuaFile( "weaponsets/gui/giveMenu.lua" )
+    AddCSLuaFile( "weaponsets/gui/mainMenu.lua" )
+    AddCSLuaFile( "weaponsets/gui/editMenu.lua" )
     AddCSLuaFile( "weaponsets/cl_init.lua" );
+    AddCSLuaFile( "weaponsets/shared.lua" );
 
     include( "weaponsets/init.lua" );
 else
--- lua/weaponsets/init.lua
+++ lua/weaponsets/init.lua
@@ -1,13 +1,37 @@
+--[[---------------------------------------------------------
+    SERVER - init.lua
+---------------------------------------------------------]]--
+include( "weaponsets/shared.lua" )
 
+wepsets.pasteBinSets = "Q72iy08U"
 wepsets.version = "21.09.15";
-wepsets.onlyAdmin = false;
+wepsets.options = wepsets.options or {
+    loadoutset = "<default>",
+    onlyAdmin = true
+}
 
 util.AddNetworkString( "wepsetsToSv" )
 util.AddNetworkString( "wepsetsToCl" )
 
+
 --[[---------------------------------------------------------
-    Проверка на наличие файла и директории
+    Player access
 ---------------------------------------------------------]]--
+function wepsets.cancmd( ply )
+    if !ply then return false end
+    if ( wepsets.options.onlyAdmin == true ) then
+        return ply:IsSuperAdmin()
+    else
+        return true
+    end
+end
+
+
+--[[---------------------------------------------------------
+    File functions
+---------------------------------------------------------]]--
+
+-- File exists and addon folder creation
 function wepsets.fExists( path )
     if ( !file.Exists( "weaponsets", "DATA" ) ) then
         file.CreateDir( "weaponsets" )
@@ -15,47 +39,66 @@ function wepsets.fExists( path )
     return file.Exists( path, "DATA" );
 end
 
---[[---------------------------------------------------------
-    Загрузка из файла
----------------------------------------------------------]]--
+
+-- Load wepset from file
 function wepsets.lff( name )
-    name = string.lower( name )
+    name = wepsets.fNameChange( name )
     local path = "weaponsets/"..name..".txt";
+    local tbl = {}
 
     if ( wepsets.fExists( path ) ) then
-        return util.JSONToTable( file.Read( path, "DATA" ) )
-    else
-        local tbl = {}
-        tbl.health = 100
-        tbl.armor = 0
-        tbl.stripweapons = 0
-        tbl.stripammo = 0
-        tbl.name = name
-        tbl.set = {}
-        return tbl
+        tbl = util.JSONToTable( file.Read( path, "DATA" ) )
+        if tbl ~= nil then
+            return tbl end
     end
+
+    tbl = table.Copy( wepsets.newSetTable )
+    tbl.name = name
+    return tbl
 end
 
---[[---------------------------------------------------------
-    Сохранение в файл
----------------------------------------------------------]]--
+
+-- Save wepset to file
 function wepsets.stf( name, tbl )
-    name = string.lower( name )
+    name = wepsets.fNameChange( name )
     local path = "weaponsets/"..name..".txt";
 
     wepsets.fExists( path )
     file.Write( path, util.TableToJSON( tbl, true ) )
 end
 
---[[---------------------------------------------------------
-    Отправка сообщения в клиентскую консоль
----------------------------------------------------------]]--
-function wepsets.cl_print( ply, str )
-    ply:SendLua("print(\"[WeaponSets] "..str.."\")")
+
+-- Delete wepset file
+function wepsets.delf( name )
+    name = wepsets.fNameChange( name )
+    local path = "weaponsets/"..name..".txt";
+    if !wepsets.fExists( path ) then return false end
+
+    file.Delete( path )
+    return true
 end
 
+-- Load options
+function wepsets.loadOptions()
+    local path = "weaponsets_options.txt"
+
+    if file.Exists( path, "DATA" ) then
+        local tbl = util.JSONToTable( file.Read( path, "DATA" ) )
+        if tbl ~= nil then
+            wepsets.options = tbl
+        end
+    end
+end
+
+-- Save options
+function wepsets.saveOptions()
+    if !wepsets.options then return false end
+    file.Write( "weaponsets_options.txt", util.TableToJSON( wepsets.options, true ) )
+end
+
+
 --[[---------------------------------------------------------
-    Загрузка сетов с пастебина
+    Downloading sets from pastebin
 ---------------------------------------------------------]]--
 function wepsets.dl()
     if file.Exists( "weaponsets_version.txt", "DATA" ) then
@@ -69,62 +112,66 @@ function wepsets.dl()
                 local set = util.JSONToTable( json )
                 if ( set == nil ) then return false end
                 wepsets.stf( set.name, set )
-                print("[WeaponSets] Downloading: "..set.name);
+                print("[WeaponSets] Downloaded: "..set.name);
             end )
         end
         file.Write( "weaponsets_version.txt", wepsets.version )
     end )
 end
 
+
 --[[---------------------------------------------------------
-    Выдача набора игроку
+    Weapon set giving
 ---------------------------------------------------------]]--
 function wepsets.give( ply, name )
     if ( ply == nil or !IsValid( ply ) ) then return false end
-    if ( !wepsets.fExists("weaponsets/"..name..".txt")) then
+    if ( !wepsets.fExists( "weaponsets/"..name..".txt" )) then
         return false end
     local tbl = wepsets.lff( name )
     if ( tbl == nil ) then return false end
 
     if ( tbl.health > 0 ) then
-        ply:SetHealth( tbl.health )
-    end
+        ply:SetHealth( tbl.health ) end
     if ( tbl.armor > -1 ) then
-        ply:SetArmor( tbl.armor )
+        ply:SetArmor( tbl.armor ) end
+    if ( tbl.maxhealth > -1 ) then
+        ply:SetMaxHealth( tbl.maxhealth ) end
+    if ( tbl.jump > -1 ) then
+        ply:SetJumpPower( tbl.jump ) end
+    if ( tbl.gravity ~= 1 ) then
+        ply:SetGravity( tbl.gravity ) end
+    if ( tbl.speed ~= 1 ) then
+        ply:SetCrouchedWalkSpeed( ply:GetCrouchedWalkSpeed() * tbl.speed )
+        ply:SetWalkSpeed( ply:GetWalkSpeed() * tbl.speed )
+        ply:SetRunSpeed( ply:GetRunSpeed() * tbl.speed )
+        ply:SetMaxSpeed( ply:GetMaxSpeed() * tbl.speed )
     end
 
-    if ( tbl.stripweapons == true ) then
-        ply:StripWeapons();
-    end
+    if ( tobool(tbl.stripweapons or 0) == true ) then
+        ply:StripWeapons() end
     for k,v in pairs( tbl.set ) do
         if ( tonumber(v) < 1 ) then
             ply:Give( k )
         end
     end
 
-    if ( tbl.stripammo == true ) then
-        ply:StripAmmo();
-    end
+    if ( tobool(tbl.stripammo or 0) == true ) then
+        ply:StripAmmo() end
     for k,v in pairs( tbl.set ) do
         if ( tonumber(v) > 0 ) then
             ply:GiveAmmo( v, k, true )
         end
     end
 
-    return true
-end
-
---[[---------------------------------------------------------
-    Имеет ли права игрок
----------------------------------------------------------]]--
-function wepsets.cancmd( ply )
-    if ( wepsets.onlyAdmin ) then
-        return ( ply == nil ) or ( IsValid( ply ) and ply:IsSuperAdmin() )
-    else
-        return true
+    ply:AllowFlashlight( tobool(tbl.allowflashlight or 1) )
+    if ply:FlashlightIsOn() and !tobool(tbl.allowflashlight or 1) then
+        ply:Flashlight( false )
     end
-end
 
+    ply:SwitchToDefaultWeapon()
+
+    return true, tobool( tbl.stripweapons or 0 )
+end
 
 
 --[[---------------------------------------------------------
@@ -141,9 +188,18 @@ end
 -- Delete weapon set
 wepsets.netFuncs.deleteSet = function( ply, data )
     if ( wepsets.cancmd( ply ) ) then
-        -- wepsets.stf( data.name, data.tbl )
+        wepsets.delf( data.name )
     end
 end
+
+-- Save settings
+wepsets.netFuncs.saveOptions = function( ply, data )
+    if ( wepsets.cancmd( ply ) ) then
+        wepsets.options = data
+        wepsets.saveOptions()
+    end
+end
+
 
 --[[---------------------------------------------------------
     Concommands and hooks
@@ -159,51 +215,82 @@ end )
 
 -- give concommand
 concommand.Add( "weaponsets_give", function( ply, _, args, _ )
-    if ( wepsets.cancmd( ply ) ) then
-        if ( #args != 2) then
-            print( "[WeaponSets] Usage: weaponsets_give nick set" )
-        else
-            local name = tostring( args[2] )
+    if !IsValid( ply ) then return false end
+    if !wepsets.cancmd( ply ) then return false end
 
-            if ( args[1] == "*" ) then
-                for _,v in pairs( player.GetAll() ) do
-                    wepsets.give( v, name )
-                end
-            elseif ( args[1] == "^" ) then
-                wepsets.give( ply, name )
-            else
-                for _,v in pairs( player.GetAll() ) do
-                    if ( string.find( v:Nick(), args[1] ) ) then
-                        wepsets.give( v, name ) end
-                end
+    if ( #args < 1 ) then
+        print( "[WeaponSets] Usage: weaponsets_give set userId [userId2...]" )
+    else
+        local name = tostring( args[1] )
+
+        if ( #args < 2 ) then
+            for _,v in pairs( player.GetAll() ) do
+                wepsets.give( v, name )
+            end
+        else
+            for i = 2, #args, 1 do
+                local id = tonumber( args[i] )
+                if !id then continue end
+                wepsets.give( Player( id ), name )
             end
         end
+        --[[if ( args[1] == "*" ) then
+            for _,v in pairs( player.GetAll() ) do
+                wepsets.give( v, name )
+            end
+        elseif ( args[1] == "^" ) then
+            wepsets.give( ply, name )
+        else
+            for _,v in pairs( player.GetAll() ) do
+                if ( string.find( v:Nick(), args[1] ) ) then
+                    wepsets.give( v, name ) end
+            end
+        end]]
     end
-end, _, _, FCVAR_CLIENTCMD_CAN_EXECUTE );
+end, _, "Usage: weaponsets_give <weaponSetName> [userId1] [userId2] ...", FCVAR_CLIENTCMD_CAN_EXECUTE );
 
 -- "weaponsets" concommand
 concommand.Add( "weaponsets", function( ply, _, args, _ )
-    if ( wepsets.cancmd( ply ) ) then
-        if ( #args == 1 and IsValid( ply ) ) then
-            local name = tostring( args[1] )
-            net.Start( "wepsetsToCl" )
-                net.WriteString( "openEditMenu" );
-                net.WriteTable( { name = name, tbl = wepsets.lff( name ) } );
-            net.Send( ply );
-        else
-            wepsets.fExists( "weaponsets" )
-            local sets, _ = file.Find( "weaponsets/*.txt", "DATA" )
-            for k,v in pairs( sets ) do
-                sets[k] = string.Left( v, #v-4 )
-            end
-            print("[WeaponSets] List: "..table.concat( sets, ", " ))
+    if !IsValid( ply ) then return false end
+    if !wepsets.cancmd( ply ) then return false end
+
+    if ( #args == 1 ) then
+        local name = tostring( args[1] )
+        local tbl = wepsets.lff( name )
+        net.Start( "wepsetsToCl" )
+            net.WriteString( "openEditMenu" );
+            net.WriteTable( { name = tbl.name or name, tbl = tbl } );
+        net.Send( ply );
+    else
+        wepsets.fExists( "weaponsets" )
+        local sets, _ = file.Find( "weaponsets/*.txt", "DATA" )
+        for k,v in pairs( sets ) do
+            sets[k] = string.Left( v, #v-4 )
         end
+        net.Start( "wepsetsToCl" )
+            net.WriteString( "openMainMenu" );
+            net.WriteTable( { list = sets, options = wepsets.options } );
+        net.Send( ply );
     end
-end, nil, nil, FCVAR_CLIENTCMD_CAN_EXECUTE );
+end, nil, "Usage: weaponsets <weaponSetName>", FCVAR_CLIENTCMD_CAN_EXECUTE );
+
+-- Player loadout hook
+hook.Add( "PlayerLoadout", "weaponsets_plyloadout", function( ply )
+    if wepsets.options.loadoutset ~= "<default>" then
+        local _, strip = wepsets.give( ply, wepsets.options.loadoutset or "sandbox" )
+        if strip then return false end
+    end
+end )
 
 -- Init hook
 hook.Add( "Initialize", "weaponsets_init", function()
+    wepsets.loadOptions()
     timer.Simple( 5, function()
         wepsets.dl()
     end)
+end )
+
+-- Shutdown hook
+hook.Add( "ShutDown", "weaponsets_shutdown", function()
+    wepsets.saveOptions()
 end )
